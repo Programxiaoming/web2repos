d3dec82158aa46a98a6565248fab33a9fec9c3df
Programxiaoming <xiaomingsu@outlook.com>
2022-11-23 22:43:13 -0500

    Rename subfolder

---
+++ Exercise1/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return <div>Hello, 欢迎来到加拿大!</div>
+
+    }
+    export default App;
--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-    return <div>Hello World Xiaoming Su!</div>
-
-    }
-    export default App;
